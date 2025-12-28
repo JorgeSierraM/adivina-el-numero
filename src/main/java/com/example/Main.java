b804b0c2dcb219b37013bf50e2d344c91313d452
JorgeSierraM <jdj.sierram@gmail.com>
2025-12-27 20:13:33 -0500

    first commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,54 @@
+package com.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        final int NUMERO_MAX_INTENTOS = 7;
+        int intentos;
+        int respuesta;
+
+        int numeroSecreto = random.nextInt(10) + 1;
+
+        System.out.println("Adivina El Numero");
+        intentos = NUMERO_MAX_INTENTOS;
+        while (intentos <= NUMERO_MAX_INTENTOS) {
+            System.out.println("=======================");
+            System.out.println("Tienes: " + intentos + " intentos");
+            System.out.print("El numero esta entre 1 y 10, cual es? ");
+            respuesta = sc.nextInt();
+
+
+            if (respuesta == numeroSecreto) {
+                System.out.println("-----------------------------------");
+                System.out.println("-----------------------------------");
+                System.out.println("El numero que ingresaste es el correcto.");
+                System.out.println("-----------------------------------");
+                System.out.println("-----------------------------------");
+                break;
+            } else if (respuesta < numeroSecreto) {
+                System.out.println("-----------------------------------");
+                System.out.println("El numero secreto es mayor.");
+                intentos--;
+            } else {
+                System.out.println("-----------------------------------");
+                System.out.println("El numero secreto es menor.");
+                intentos--;
+            }
+
+            if (intentos == 0) {
+                System.out.println("-----------------------------------");
+                System.out.println("-----------------------------------");
+                System.out.println("Perdiste, te quedaste sin intentos.");
+                System.out.println("El numero secreto era: " + numeroSecreto);
+                System.out.println("-----------------------------------");
+                System.out.println("-----------------------------------");
+                break;
+            }
+        }
+
+    }
+}
